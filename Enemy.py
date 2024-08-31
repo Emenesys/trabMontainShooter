0194f2a003b2da4e395f08d8907bf2e194339b97
CLEBER JOSE COSTA <48730510+Emenesys@users.noreply.github.com>
2024-08-31 19:11:24 -0300

    Added stage 3 of the game and included movement

---
+++ Enemy.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import math
+
+from code.Const import ENTITY_SPEED, ENTITY_SHOT_DELAY, WIN_HEIGHT, WIN_WIDTH
+from code.EnemyShot import EnemyShot
+from code.Entity import Entity
+
+
+class Enemy(Entity):
+    def __init__(self, name: str, position: tuple):
+        super().__init__(name, position)
+        self.shot_delay = ENTITY_SHOT_DELAY[self.name]
+
+    def move(self):
+        if self.name == 'Enemy3':
+            self.rect.centerx -= ENTITY_SPEED[self.name]
+            self.rect.centery += int(4 * math.sin(0.02 * self.rect.centerx))  # move in "zigue-zague"
+            if self.rect.top < 0:  # Limit top
+                self.rect.top = 0
+            elif self.rect.bottom > WIN_HEIGHT:  # Limit bottom
+                self.rect.bottom = WIN_HEIGHT
+
+        else:
+            self.rect.centerx -= ENTITY_SPEED[self.name]
+
+    def shoot(self):
+        self.shot_delay -= 1
+        if self.shot_delay == 0:
+            self.shot_delay = ENTITY_SHOT_DELAY[self.name]
+            return EnemyShot(name=f'{self.name}Shot', position=(self.rect.centerx, self.rect.centery))
